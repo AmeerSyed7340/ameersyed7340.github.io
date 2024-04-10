cde53fac9de36889e92fad4f945d0e1985331909
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-04-10 04:28:56 -0400

    Initial commit, set up react

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className='min-w-[100px] min-h-[50px] max-w-[100px] mx-[50px] bg-slate-900 hover:bg-slate-300 duration-1000'></div>
+    </>
+  );
+}
+
+export default App;
