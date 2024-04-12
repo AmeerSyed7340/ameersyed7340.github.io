edebcc57cc020ab9c4545de792a3e3910a72c71d
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-04-12 07:25:01 -0400

    Added social links

---
+++ client/src/components/Socials.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+const Socials = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={25} />
+        </>
+      ),
+      href: "https://linkedin.com",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={25} />
+        </>
+      ),
+      href: "https://github.com/AmeerSyed7340",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={25} />
+        </>
+      ),
+      href: "mailto:ameersyed60@gmail.com",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={25} />
+        </>
+      ),
+      href: "/AmeerSyed_Resume.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+
+  return (
+    <div className="flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        {links.map(({ id, child, href, download, style }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-105px] hover:ml-[-5px] hover:rounded-md duration-300 bg-gray-500" +
+              " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Socials;
