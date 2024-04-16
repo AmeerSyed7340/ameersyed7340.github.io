8f59aa34454a823440cbccb48c84e81ea01f1d88
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-04-16 05:32:35 -0400

    Set up complate portfolio with placeholder projects and working getform.io

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
         </div>
 
         <div className='flex justify-center items-center'> 
-          <form action="" method="POST" className='flex flex-col w-full md:w-1/2'>
+          <form action="https://getform.io/f/paqgedza" method="POST" className='flex flex-col w-full md:w-1/2'>
             <input
               type="text"
               name="name"
